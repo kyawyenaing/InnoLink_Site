f66ec554c4ef7a087f6dcb3ac0875d9b8be421a3
kyawyenaing <thushin27@gmail.com>
2017-03-14 15:44:59 +0630

    update salary api

--- app/models/salary_range.rb
+++ app/models/salary_range.rb
@@ -2,7 +2,7 @@ class SalaryRange < ActiveRecord::Base
 
 	# for api
 	  def self.get_api(name)
-	    City.order(created_at: :DESC)        
+	    SalaryRange.order(created_at: :DESC)        
 	  end
 	# end api
 end
