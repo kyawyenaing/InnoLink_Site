fea7fd582e6a0871c407d51cf5af37d79dc6e559
kyawyenaing <thushin27@gmail.com>
2017-03-20 10:44:40 +0630

    api for new job

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -8,4 +8,34 @@ class Api::JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+  	@job = Job.new
+  	@cities = City.get_list
+  	# redirect_to new_job_path
+  end
+
+  def create
+  	@job = Job.new(job_params)
+  	@job.user_id = current_user.id
+  	if @job.save
+    redirect_to jobs_path, notice: 'Job was successfully created.'
+  	else
+    redirect_to new_job_path 
+  	end
+	end
+
+	def edit
+	  @job = Job.find(params[:id])    
+	  @city = City.get_list
+	  redirect_to edit_job_path
+	end
+
+	private
+	def job_params
+	  params.require(:job).permit(:title, :company_name, :company_website, :job_type, 
+	  :category_id, :salary_range_id, :city_id, :description, :requirement, 
+	  :how_to,:user_id)
+	end
+
+
 end
---
+++ app/views/api/jobs/new.json.jbuilder
@@ -0,0 +1,12 @@
+json.id @job.id
+json.title @job.title
+json.company_name @job.company_name
+json.company_website @job.company_website
+json.job_type @job.get_job_type
+json.category_id @job.category_id
+json.salary_range_id @job.salary_range_id
+json.city_id @job.city_id
+# json.city_id @job.city_id
+json.description @job.description
+json.requirement @job.requirement
+json.how_to @job.how_to
