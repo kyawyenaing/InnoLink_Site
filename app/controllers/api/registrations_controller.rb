5c4bd55e946a6b9e41d6202d19cbe8b4f7e8af56
kyawyenaing <thushin27@gmail.com>
2017-04-10 10:28:13 +0630

    update reg api

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -14,7 +14,7 @@ class Api::RegistrationsController < Devise::RegistrationsController
                       :data => { :user => resource,
                                  :auth_token => current_user.authentication_token } }
     else
-      render :status => :unprocessable_entity,
+      render :status => 200,
              :json => { :success => false,
                         :info => resource.errors,
                         :data => {} }
