5a9f443a3fad6b5458db3184ae4723f221e1baa1
kyawyenaing <thushin27@gmail.com>
2017-05-03 15:21:49 +0630

    test by removing ! in session api

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,10 @@
 # file: app/controller/api/v1/sessions_controller.rb
 class Api::SessionsController < Devise::SessionsController
-  skip_before_filter :verify_authenticity_token,
-                     :if => Proc.new { |c| c.request.format == 'application/json' }
-
+  
   respond_to :json
 
   def create
-    warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+    warden.authenticate(:scope => resource_name, :recall => "#{controller_path}#failure")
     render :status => 200,
            :json => { :success => true,
                       :info => "Login Success!",
