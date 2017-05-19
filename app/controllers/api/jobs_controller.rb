ae9c7b2e145c90c55ebb680fbfd6391f169bf354
kyawyenaing <thushin27@gmail.com>
2017-05-19 10:57:31 +0630

    update some bugs and test for git repo

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -51,9 +51,13 @@ class Api::JobsController < ApplicationController
 
 	private
 	def job_params
-	  params.require(:job).permit(:title, :company_name, :company_website, :job_type, 
-	  :category_id, :salary_range_id, :city_id, :description, :requirement, 
-	  :how_to,:user_id,:company_id)
+	  # params.require(:job).permit(:title, :company_name, :company_website, :job_type, 
+	  # :category_id, :salary_range_id, :city_id, :description, :requirement, 
+	  # :how_to,:user_id,:company_id)
+
+    params.require(:job).permit(:title, :company_name, :company_website, :job_type, 
+    :category_id, :salary_range_id, :city_id, :description, :requirement, 
+    :how_to,:company_id)
 	end
 
 
