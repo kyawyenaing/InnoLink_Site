8246e45d3314ef0534963eb335cd7599e2d58737
kyawyenaing <thushin27@gmail.com>
2017-07-15 11:31:17 +0630

    add status

--- app/models/boostcampaing.rb
+++ app/models/boostcampaing.rb
@@ -1,2 +1,5 @@
 class Boostcampaing < ActiveRecord::Base
+	belongs_to :job
+	enum status: { PENDING: 0, CONFIRM: 1}
+	validates_presence_of :job_id, :start_date, :end_date, :status
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714091505) do
+ActiveRecord::Schema.define(version: 20170715040447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20170714091505) do
     t.date     "start_date"
     t.date     "end_date"
     t.integer  "total_budget"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "status",       default: 0, null: false
   end
 
   create_table "boostedjobs", force: :cascade do |t|
